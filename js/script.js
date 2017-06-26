1b7c72eb07b98610f27a191d1b88a4b8023e500b
Dennis <hydennis@users.noreply.github.com>
2017-06-26 19:42:05 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,50 +1,119 @@
-var st, lst = window.pageYOffset
-	mobileWidth = 480,
-	slideTimer = 400;
-	
-;(function(){
-	/*bindMenuShowHide();*/
-	
-	$(window).resize(function(){
-		var ww = $(document).width(),
-			isMobileViewport = ww <= mobileWidth ? true : false;
-		
-		if(!isMobileViewport){
-			$("nav").show();
-		}
-	});
-	
-	$('#content').pagepiling({
-		menu: 'nav',
-		anchors: ['home', 'about', 'showcase', 'contact'],
-        sectionSelector: 'section',
-        scrollingSpeed: 400,
-		navigation: {
-            'textColor': '#000',
-            'bulletsColor': 'transparent',
-            'position': 'right',
-            'tooltips': ['home', 'about', 'showcase', 'contact']
+var particlesJSON = {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
         },
-	});
-}());
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 10,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 80,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 300,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 2
+        },
+        "move": {
+            "enable": true,
+            "speed": 12,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 800,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 800,
+                "size": 80,
+                "duration": 2,
+                "opacity": 0.8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 400,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+};
 
-function bindMenuShowHide(){
-	$(window).scroll(function(){
-		var ww = $(document).width(),
-			isMobileViewport = ww <= mobileWidth ? true : false;
-		
-		if(isMobileViewport){
-			st = window.pageYOffset;
-			
-			if (st > lst) {
-				$("nav").slideUp(slideTimer);
-			} else {			
-				$("nav").slideDown(slideTimer);
-			}
-			
-			lst = st;
-		} else {
-			$("nav").show();
-		}
-	});
-}
+particlesJS.load('landing', particlesJSON, function() {
+    // console.log('callback - particles.js config loaded');
+});
+
+;
+(function() {
+
+}());
