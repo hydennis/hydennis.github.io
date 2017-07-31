99896d1589a2c286bf48b07b8fb8e0381f838864
Dennis <hydennis@users.noreply.github.com>
2017-07-31 19:51:29 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-particlesJS.load('landing', 'js/particlesjs-config.json', function() {
-	// console.log('callback - particles.js config loaded');
-});
+var min = 200;
+//particlesJS.load('landing', 'js/particlesjs-config.json', function() {
+    // console.log('callback - particles.js config loaded');
+//});
 
-;(function(){
-	
+;(function() {
+	$(".block").each(function(){
+		$(this).width(min + Math.random()*100).height(min + Math.random()*100);
+	});
+
+    var wall = new Freewall("#showcaseContent");
+    wall.reset({
+        selector: '.block',
+        animate: true,
+        cellW: 100,
+        cellH: 100,
+        onResize: function() {
+            wall.fitWidth();
+        }
+    });
+    wall.fitWidth();
 }());
